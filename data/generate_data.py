e825f68eafa6f0e42b3a11ed700ead2d722ab1f2
Matthijs Keep <mdkeep@gmail.com>
2025-11-02 20:24:12 +0100

    docker works up to saving (but not included)

--- data/generate_data.py
+++ data/generate_data.py
@@ -3,6 +3,9 @@ import numpy as np
 from datetime import datetime, timedelta
 import random
 
+import pyarrow as pa
+schema = pa.schema([('timestamp', pa.timestamp('us')), ('category', pa.string()), ('amount', pa.float64())])
+
 np.random.seed(42)
 n_transactions = 10000
 start_date = datetime(2024, 1, 1)
@@ -13,6 +16,9 @@ df = pd.DataFrame({'timestamp': dates, 'category': categories, 'amount': amounts
 df_dupe = df.sample(frac=0.05)
 df.loc[df_dupe.index, 'category'] = np.random.choice(['Food', 'Transport'], len(df_dupe))
 df = pd.concat([df, df_dupe])
-df.to_parquet('raw_transactions.parquet', index=False)
+df['timestamp'] = df['timestamp'].astype('datetime64[us]')
+df.to_parquet('raw_transactions.parquet', index=False, coerce_timestamps="ms")
 print(f"Generated {len(df)} noisy transactions.")
+print("Schema looks like:")
+print(pa.parquet.read_schema('raw_transactions.parquet'))
 
